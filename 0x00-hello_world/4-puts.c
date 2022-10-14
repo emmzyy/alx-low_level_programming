c7503d3b83e440146e5b880e9c2ed61168117ba4
emmzyy <ibeh667@gmail.com>
2022-10-14 02:13:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints a statement
+ * a blank line
+ * Return: returns the value of the function
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
