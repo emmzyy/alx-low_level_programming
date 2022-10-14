6a8c2b5b5afcc42e4e7b822026da18e59264d53f
emmzyy <ibeh667@gmail.com>
2022-10-14 06:03:44 -0700

    advance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-fwrite(STDOUT_FILENO,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fprintf(stderr,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
