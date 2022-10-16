ace1a1b7e2377ce3211a61be08efeeebfe58d5e8
emmzyy <ibeh667@gmail.com>
2022-10-16 10:37:21 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-	int x;
-	for (x = 0; x < 10; x++)
-		putchar((x % 10) + '0');
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
 	putchar('\n');
 	return (0);
