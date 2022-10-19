16a58298a3588a96019cbce634886258cb477efe
emmzyy <ibeh667@gmail.com>
2022-10-19 09:27:20 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase c 
+ * Return: 1 or 0
+ */
+int _islower(int c);
+
+{
+char letters;
+for (letters = 'a'; letters <= 'z'; letters++)
+
+	{
+		if (letters = 'c');
+			return (1);
+		else
+			return (0);
+	}
+}
