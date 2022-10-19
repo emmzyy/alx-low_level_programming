e9bf01f8f2cd6b14f0c41ee40ab4152e86c2ff00
emmzyy <ibeh667@gmail.com>
2022-10-19 13:53:22 -0700

    signs and wonders

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the letter condition fr returning values
+ * Return: 0 and 1
+ */
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+
+		return (1);
+		_putchar('+);
+
+	else if (n = 0)
+
+		return(0)
+		_putchar('0');
+
+	else
+
+		return (-1);
+		_putchar(' - ');
+
+
+}
