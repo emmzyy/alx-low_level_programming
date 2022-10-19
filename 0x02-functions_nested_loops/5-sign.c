97e2090249677fce700e43748ccd1bb0c7b5c5c1
emmzyy <ibeh667@gmail.com>
2022-10-19 13:59:36 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,22 +7,19 @@
  */
 int print_sign(int n)
 {
-	int n;
-
 	if (n > 0)
 
+		_putchar('+');
 		return (1);
-		_putchar('+);
 
 	else if (n = 0)
 
-		return(0)
 		_putchar('0');
+		return(0);
 
 	else
 
-		return (-1);
 		_putchar(' - ');
-
+		return (-1);
 
 }
