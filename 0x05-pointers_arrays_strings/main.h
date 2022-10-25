28854b5827603f6d53961692aeafee6e58278105
emmzyy <ibeh667@gmail.com>
2022-10-25 13:52:48 -0700

    check errors

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 void reset_to_98(int *n);
@@ -11,6 +11,8 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 int _putchar(char c);
+
 
 #endif
