147cdcf402f3e9816859633d050b4ccd70e816f7
emmzyy <ibeh667@gmail.com>
2022-10-25 13:05:31 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,14 +6,14 @@
  *
  * Return: the length of the string
  */
+
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		i++;
 	}
 	_putchar('\n');
 }
